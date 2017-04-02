9f6059b6c8048a3c72414d959d7526393854754b
Daniel_Lawliet <daniel_lawliet@qq.com>
2017-04-02 21:37:15 +0800

    Create spider.py

---
+++ reptile/lottery/spider.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+import html_downloader
+import html_parser
+import html_output
+
+
+class SpiderMain(object):
+    def __init__(self):
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.output = html_output.HtmlOutput()
+
+    def craw(self):
+        for i in range(1,106):
+            url = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_%d.html' % i
+            print 'craw %d :%s' % (i, url)
+            html_cont = self.downloader.download(url)
+            rdata, bdata, mdate = self.parser.parse(html_cont)
+            self.output.collect_data(rdata ,bdata, mdate)
+
+        self.output.output_text()
+
+if __name__ == '__main__':
+    obj_spider = SpiderMain()
+    obj_spider.craw()
