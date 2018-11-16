11162414d11187dc19d6d1d279867ce562cacb5e
wangpan <daniel_lawliet@qq.com>
2018-11-16 18:07:25 +0800

    补充提交

--- reptile/scrapy/mytest/mytest/spiders/lottery.py
+++ reptile/scrapy/mytest/mytest/spiders/lottery.py
@@ -16,34 +16,27 @@ class LotterySpider(scrapy.Spider):
 			yield scrapy.Request(url=url, callback=self.parse)
  
 	def parse(self, response):
-		print(response)
 		if  response is None:
 			return
 		soup = BeautifulSoup(response.text,'html.parser')
-		self._get_new_data(soup)
+		items = self._get_new_data(soup)
+		return items
 		
 	def _get_new_data(self, soup):
 		#根据页面元素匹配获取数字
 		item = MytestItem()
-		
+		items = []
+
 		r_balls = soup.find_all('em', class_="rr")
+		b_ball = re.findall(r'<em>(\d{2})</em>', str(soup))
+		mydate = re.findall(r'align="center">(\d{4}-\d{2}-\d{2})', str(soup))
 
 		for i in range(len(r_balls)):
 			temp = i%6
 			item['red_%d'%temp]=(r_balls[i].get_text())
-		
-		b_ball = soup.find_all('em', class_="")
-		for data in b_ball:
-			item['blue']=(data.get_text())
-			print(item['blue'])
-		
-		mydate = soup.find_all('td', align='center')
-		for edate in mydate:
-			pattern = re.compile(r"[0-9]{7}")
-			match = pattern.findall(edate.get_text())
-			if match:
-				item['m_date']=(match.pop())
-		
-		print(item['blue'])
+			if temp == 5:
+				item['blue']=b_ball.pop(0)
+				item['m_date']=mydate.pop(0)
+				items.append(item)
 
 		return item
