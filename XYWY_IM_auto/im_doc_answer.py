560d71190328f0919f1f13a17d1ac824836cebf8
Daniel_Lawliet <daniel_lawliet@qq.com>
2018-12-11 23:10:58 +0800

    医生回复代码更新

--- XYWY_IM_auto/im_doc_answer.py
+++ XYWY_IM_auto/im_doc_answer.py
@@ -11,15 +11,11 @@ class login(Page):
 	'''
 	医生端i、抢题、回答
 	'''
-	answer_num = 2
-
-	login_username_loc = (By.NAME, 'userName')
-	login_password_loc = (By.NAME, 'password')
-	login_button_loc = (By.CLASS_NAME, 'bc39')
-
-	def login_doctor(self, did=117333219):
+	def __init__(self, did=117333219):
 		#医平台登录
 		url_login = 'http://test.dr.xywy.com/site/login'
+		url_doc = 'http://test.dr.xywy.com/site/login'
+		url_im = 'http://test.dr.xywy.com/site/login'
 		#传入的user_id查找页
 		self.headers = {
 		"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
@@ -47,44 +43,38 @@ class login(Page):
 		rob_qid = requests.get(url_rob, headers=self.headers, cookies=self.doc_im_cookies)
 		print(rob_qid.text)
 
-
-	def answer_question(self, qid, is_summary):
-		handles = self.driver.window_handles
-		self.driver.switch_to_window(handles[0])
-		sleep(1)
-		self.driver.refresh()
-		#点击处理中
-		self.driver.find_element_by_xpath('//*[@class="message-status pr fYaHei clearfix"]/div[1]')
-		self.Load_button()
-		#点击处理中问题
-		My_questions = self.driver.find_elements_by_class_name('message-user-item')
-		for i in My_questions:
-			if int(i.get_attribute('data-qid')) == qid:
-				i.click()
-				self.Load_button()
-				break
-		#快速问答设置为An
-		An = self.driver.find_element_by_xpath('//*[@class="message-quick-list clearfix fYaHei"]/li[1]')
-		self.Load_button()
-		#点击快速回答
+class answer_question():
+	def __init__(self, qid, is_summary):
+		self.qid = qid
+		websocket.enableTrace(True)
+		self.ws = websocket.WebSocketApp("ws://10.20.4.22:8078/websocket",on_message = on_message,on_error = on_error,on_close = on_close)
+		self.ws.on_open = self.on_open
 		for i in range(4):
-			An.click()
-			sleep(0.5)
+			self.reply(1)
 		if is_summary == 0:
 			print('不写总结')
 		else:
 			print('写总结')
-			self.driver.find_element_by_xpath('//*[@class="pr tc f14 fYaHei message-select"]/span[2]').click()
-			sleep(1)
-			self.driver.find_element_by_id('zzs').send_keys('请描述患者症状，详细分析该症状是什么疾病，引发这种疾病或症状的原因是什么')
-			self.driver.find_element_by_id('jys').send_keys('针对问题分析，尽可能详细的给出用户各种意见建议，包含：就诊建议、挂号科室建议、检查建议、用药建议、日常注意事项及护理建议等')
-			self.driver.find_element_by_xpath('//*[@class="summary-send-btn f14 fYaHei"]').click()
-			sleep(1)
+			pass
+		ws.run_forever()
 
-	def answer_ques_20(self, times):
-		self.driver.find_element_by_name('message').clear()
-		self.driver.find_element_by_name('message').send_keys('医生回复%d' %times)
-		self.driver.find_element_by_name('message').send_keys(Keys.ENTER)
+	def reply(self, ws, times):
+		self.ws.send('{"from": "68258667","to": "333","id": "%d","body": {"content": "回复内容%d","qid": "%d"},"act": "PUB"}'%(int(round(time.time() * 1000))),times,%self.qid)
+
+	def on_message(ws, message):
+		print(message)
+		if json.loads(message)['act'] == "PING":
+			self.ws.send('{"act":"PONG"}')
+
+	def on_error(ws, error):
+		print(error)
+
+	def on_close(ws):
+		print("### closed ###")
+
+	def on_open(ws):
+		self.ws.send('{"userid": "68258667", "act": "CONNECT"}')
+
 
 if __name__ == '__main__':
 	A = login()
