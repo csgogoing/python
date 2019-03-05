9fae486ba6ce3e09932f3976519d6cf3422ec9c1
Daniel_Lawliet <daniel_lawliet@qq.com>
2019-03-05 23:39:43 +0800

    指定

--- statistic/statistics_im.py
+++ statistic/statistics_im.py
@@ -40,6 +40,7 @@ class Statistics_Im(object):
 	def get_data(self):
 		self.im_login()
 		url = "http://admin.d.xywy.com/order/question/index"
+		#悬赏
 		#寻医问药app
 		reward_xywyapp_unpaid = {
 			'QuestionOrderSearch[order_type]' : '2',
@@ -332,15 +333,292 @@ class Statistics_Im(object):
 		else:
 			print('IM悬赏统计完成')
 
-		# try:
-		# 	request_reward_xywyapp_unpaid = requests.get(url, params=reward_xywyapp_unpaid, headers=self.headers, cookies=self.cookies).text
-		# 	self.get_num_unpaid(3, 14, request_reward_xywyapp_unpaid)
-		# 	request_reward_xywyapp_paid = requests.get(url, params=reward_xywyapp_paid, headers=self.headers, cookies=self.cookies).text
-		# 	self.get_num_paid(3, 14, request_reward_xywyapp_paid)
-		# except Exception as e:
-		# 	print(e)
-		# else:
-		# 	print('IM悬赏统计完成')
+		#指定
+		#寻医问药app
+		assign_xywyapp_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '2',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_xywyapp_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '2',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#问医生app
+		assign_askapp_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '11',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_askapp_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '11',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#百度熊掌号
+		assign_bdxzh_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '16',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_bdxzh_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '16',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#熊掌号底bar
+		assign_bdxzh_bar_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '22',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_bdxzh_bar_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '22',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#熊掌号mip
+		assign_bdxzh_mip_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '23',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_bdxzh_mip_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '23',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#wap
+		assign_3g_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '4',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_3g_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '4',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#微信小程序
+		assign_weixin_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '25',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_weixin_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '25',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#百度
+		assign_baidu_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '1',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_baidu_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '1',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#华夏保险
+		assign_hxbx_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '19',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_hxbx_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '19',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#搜狗
+		assign_sougou_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '26',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_sougou_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '26',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		#快应用
+		assign_kuaiyingyong_unpaid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '14',
+			'QuestionOrderSearch[pay_status]' : '1',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		assign_kuaiyingyong_paid = {
+			'QuestionOrderSearch[order_type]' : '2',
+			'QuestionOrderSearch[pay_source]' : '14',
+			'QuestionOrderSearch[pay_status]' : '2',
+			'QuestionOrderSearch[pay_type]' : '',
+			'QuestionOrderSearch[keyword_type]' : '',
+			'QuestionOrderSearch[keyword]' : '',
+			'QuestionOrderSearch[bgDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day),
+			'QuestionOrderSearch[edDate]' : '%s-%s-%s'%(self.cur.year,self.cur.month,self.cur.day)
+			}
+		try:
+			request_assign_xywyapp_unpaid = requests.get(url, params=assign_xywyapp_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 14, request_assign_xywyapp_unpaid)
+			request_assign_xywyapp_paid = requests.get(url, params=assign_xywyapp_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 14, request_assign_xywyapp_paid)
+
+			request_assign_askapp_unpaid = requests.get(url, params=assign_askapp_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 20, request_assign_askapp_unpaid)
+			request_assign_askapp_paid = requests.get(url, params=assign_askapp_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 20, request_assign_askapp_paid)
+
+			request_assign_bdxzh_unpaid = requests.get(url, params=assign_bdxzh_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 26, request_assign_bdxzh_unpaid)
+			request_assign_bdxzh_paid = requests.get(url, params=assign_bdxzh_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 26, request_assign_bdxzh_paid)
+
+			request_assign_bdxzh_bar_unpaid = requests.get(url, params=assign_bdxzh_bar_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 32, request_assign_bdxzh_bar_unpaid)
+			request_assign_bdxzh_bar_paid = requests.get(url, params=assign_bdxzh_bar_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 32, request_assign_bdxzh_bar_paid)
+
+			request_assign_bdxzh_mip_unpaid = requests.get(url, params=assign_bdxzh_mip_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 38, request_assign_bdxzh_mip_unpaid)
+			request_assign_bdxzh_mip_paid = requests.get(url, params=assign_bdxzh_mip_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 38, request_assign_bdxzh_mip_paid)
+
+			request_assign_3g_unpaid = requests.get(url, params=assign_3g_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 44, request_assign_3g_unpaid)
+			request_assign_3g_paid = requests.get(url, params=assign_3g_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 44, request_assign_3g_paid)
+
+			request_assign_kuaiyingyong_unpaid = requests.get(url, params=assign_kuaiyingyong_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 50, request_assign_kuaiyingyong_unpaid)
+			request_assign_kuaiyingyong_paid = requests.get(url, params=assign_kuaiyingyong_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 50, request_assign_kuaiyingyong_paid)
+
+			request_assign_baidu_unpaid = requests.get(url, params=assign_baidu_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 55, request_assign_baidu_unpaid)
+			request_assign_baidu_paid = requests.get(url, params=assign_baidu_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 55, request_assign_baidu_paid)
+
+
+			request_assign_sougou_unpaid = requests.get(url, params=assign_sougou_unpaid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_unpaid(5, 60, request_assign_sougou_unpaid)
+			request_assign_sougou_paid = requests.get(url, params=assign_sougou_paid, headers=self.headers, cookies=self.cookies).text
+			self.get_num_paid(5, 60, request_assign_sougou_paid)
+
+
+
+		except Exception as e:
+			print(e)
+		else:
+			print('IM指定统计完成')
+
 
 	def get_num_unpaid(self, sheet, column, req_text):
 		self.ws = self.wb.get_sheet(sheet)
@@ -355,6 +633,16 @@ class Statistics_Im(object):
 		shifu = re.findall(r'实付金额：(.*)元</span>', req_text)[0]
 		q_num = re.findall(r'共<b>(.*)</b>条数据.</div>', req_text)
 		self.ws.write(self.row, column+2, shifu)
+		if q_num==[]:
+			self.ws.write(self.row, column, 0)
+		else:
+			self.ws.write(self.row, column, q_num[0])
+
+	def get_num_paid_l(self, sheet, column, req_text):
+		self.ws = self.wb.get_sheet(sheet)
+		shifu = re.findall(r'实付金额：(.*)元</span>', req_text)[0]
+		q_num = re.findall(r'共<b>(.*)</b>条数据.</div>', req_text)
+		self.ws.write(self.row, column+1, shifu)
 		if q_num==[]:
 			self.ws.write(self.row, column, 0)
 		else:
