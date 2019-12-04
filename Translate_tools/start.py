f4c8dc19052d1e58e8de7393a8dbfe6c07cf5b2c
Daniel_Lawliet <daniel_lawliet@qq.com>
2019-12-04 22:52:18 +0800

    翻译工具
    未完成

--- Translate_tools/start.py
+++ Translate_tools/start.py
@@ -2,23 +2,39 @@
 import requests
 import re
 import os
+import sys
+import random
+import xlrd
+import xlwt
+import time
+import datetime
 from lxml import etree
 
 #/html/body/div[3]/div[1]/div[3]/div[1]/div/a[15]
 #//@class="listA"
-
-
-def main():
-	#test()
-	urls = get_url()
-	parse_url(urls)
-
-
 def test():
-	a = ['世界的方式结合地方 sfsdfsdfs', 'sfsdfsf 斯蒂芬斯蒂芬', 'sdfdsfds', '沙发上地方是', 'dsfds sdfsd 斯蒂芬斯蒂芬多少']
+	a = ['1世界的方式结合地方 sfsdfsdfs', 'sfsdfsf 1斯蒂芬斯蒂芬', 'sdfdsfds', '沙发上地方是', 'dsfds sdfsd 斯蒂芬斯蒂芬多少']
 	for i in a:
-		if re.search(r'[\u4e00-\u9fa5]',i) and re.search(r'[a-zA-Z]',i):
+		if re.match(r'^([0-9]).*',i):
 			print(i)
+
+
+class Write_Excel():
+	#主类
+	def __init__(self, file):
+		rb = xlrd.open_workbook(file, formatting_info=True)
+		self.wb = copy(rb)
+ 
+	def save(self):
+		self.wb.save('2019年统计数据_基础服务&后台组-%d月%d日改.xls'%(cur.month,cur.day))
+	
+	#示例写入方法s
+	def write(self, sheet, row, column, result):
+		ws = self.wb.get_sheet(0)
+		ws.write(row, column, result)
+
+
+
 
 
 
@@ -53,8 +69,40 @@ def parse_url(urls):
 			if re.search(r'[\u4e00-\u9fa5]',i) and re.search(r'[a-zA-Z]',i):
 				f.write(i)
 				f.write('\n')
-
 	f.close()
+
+
+def txt_to_excel():
+	f = open('1.txt', 'r', encoding='gbk')
+	excel_path=os.getcwd()+'\\ERP.xlsx'
+	rb = xlwt.Workbook()  #新建一个Excel
+	sheet = rb.add_sheet(u'translate',cell_overwrite_ok=True) #新建sheet
+	row = 1
+	while True:
+		next = f.readline()
+		if next != '' and re.match(r'^([0-9]).*',next):
+			tmp = next.split(' ')
+			re_chn = tmp[-1].strip('\n')
+			re_eng = " ".join('%s' %id for id in tmp[1:-1])
+			# print(re_chn)
+			# print(re_eng)
+			sheet.write(row,0,re_chn)
+			sheet.write(row,1,re_eng)
+			row = row + 1
+		else:
+			break
+	rb.save(excel_path)
+	
+	#示例写入方法s
+
+
+def main():
+	#test()
+	# urls = get_url()
+	# parse_url(urls)
+	# we = Write_Excel(excel_path)
+	txt_to_excel()
+
 
 
 if __name__ == '__main__':
