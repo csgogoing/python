bf3d6bef32d4cc79523dfec6c3625aefff10159a
Daniel_Lawliet <daniel_lawliet@qq.com>
2019-12-06 15:16:30 +0800

    翻译demo版本成型
    需要优化多内容

--- Translate_tools/1.py
+++ Translate_tools/1.py
@@ -1,3 +1,39 @@
 a = [1,2,3,4]
 b = '.'.join(a)
 print(b)
+
+
+
+
+# def get_url():
+# 	host = 'http://www.dongao.com'
+# 	headers = {} 
+# 	#url_get = ['http://www.dongao.com/practice/kjyy/','http://www.dongao.com/practice/kjyy/List_2.shtml','http://www.dongao.com/practice/kjyy/List_1.shtml']
+# 	url_get = ['http://www.dongao.com/practice/kjyy/']
+# 	url_parse = []
+# 	url_result = []
+# 	for url in url_get:
+# 		req = requests.get(url=url)
+# 		req_text = req.content.decode('GBK')
+# 		tree = etree.HTML(req_text)
+# 		url_parse_t = tree.xpath('//a[@class="listA"]/@href')
+# 		url_parse = url_parse + url_parse_t
+
+# 	for url in url_parse:
+# 		url_result.append(host+url)
+# 	return(url_result)
+
+
+# def parse_url(urls):
+# 	excel_path = os.getcwd()
+# 	f = open(excel_path + '/a', 'w', encoding='utf-8')
+# 	for url in urls:
+# 		req = requests.get(url=url)
+# 		req_text = req.content.decode('GBK')
+# 		pattern = re.compile(r'<P>\u3000\u3000(.*)</P>')
+# 		word = pattern.findall(req_text)
+# 		for i in word:
+# 			if re.search(r'[\u4e00-\u9fa5]',i) and re.search(r'[a-zA-Z]',i):
+# 				f.write(i)
+# 				f.write('\n')
+# 	f.close()
--- Translate_tools/start.py
+++ Translate_tools/start.py
@@ -8,102 +8,46 @@ import xlrd
 import xlwt
 import time
 import datetime
+import json
+from xlutils.copy import copy
 from lxml import etree
-
-#/html/body/div[3]/div[1]/div[3]/div[1]/div/a[15]
-#//@class="listA"
-def test():
-	a = ['1世界的方式结合地方 sfsdfsdfs', 'sfsdfsf 1斯蒂芬斯蒂芬', 'sdfdsfds', '沙发上地方是', 'dsfds sdfsd 斯蒂芬斯蒂芬多少']
-	for i in a:
-		if re.match(r'^([0-9]).*',i):
-			print(i)
+from translate import Py4Js
 
 
-class Write_Excel():
+class Translate_Excel():
 	#主类
 	def __init__(self, file):
-		rb = xlrd.open_workbook(file, formatting_info=True)
-		self.wb = copy(rb)
+		self.rb = xlrd.open_workbook(file, formatting_info=True)
+		self.table = self.rb.sheets()[0]
+
+		self.wb = copy(self.rb)
+		self.sheet = self.wb.get_sheet(0)
+
+		self.tran_google = Py4Js()
  
-	def save(self):
-		self.wb.save('2019年统计数据_基础服务&后台组-%d月%d日改.xls'%(cur.month,cur.day))
+	def save_excel(self):
+		self.wb.save('translate_result.xls')
 	
-	#示例写入方法s
-	def write(self, sheet, row, column, result):
-		ws = self.wb.get_sheet(0)
-		ws.write(row, column, result)
+	def translate(self):
+		row = 1
+		nrows = self.table.nrows
+		print('共%s行'%nrows)
+		list_need = self.table.col_values(0, start_rowx=1, end_rowx=None)
+		for word_need in list_need:
 
-
-
-
-
-
-def get_url():
-	host = 'http://www.dongao.com'
-	headers = {} 
-	#url_get = ['http://www.dongao.com/practice/kjyy/','http://www.dongao.com/practice/kjyy/List_2.shtml','http://www.dongao.com/practice/kjyy/List_1.shtml']
-	url_get = ['http://www.dongao.com/practice/kjyy/']
-	url_parse = []
-	url_result = []
-	for url in url_get:
-		req = requests.get(url=url)
-		req_text = req.content.decode('GBK')
-		tree = etree.HTML(req_text)
-		url_parse_t = tree.xpath('//a[@class="listA"]/@href')
-		url_parse = url_parse + url_parse_t
-
-	for url in url_parse:
-		url_result.append(host+url)
-	return(url_result)
-
-
-def parse_url(urls):
-	excel_path = os.getcwd()
-	f = open(excel_path + '/a', 'w', encoding='utf-8')
-	for url in urls:
-		req = requests.get(url=url)
-		req_text = req.content.decode('GBK')
-		pattern = re.compile(r'<P>\u3000\u3000(.*)</P>')
-		word = pattern.findall(req_text)
-		for i in word:
-			if re.search(r'[\u4e00-\u9fa5]',i) and re.search(r'[a-zA-Z]',i):
-				f.write(i)
-				f.write('\n')
-	f.close()
-
-
-def txt_to_excel():
-	f = open('1.txt', 'r', encoding='gbk')
-	excel_path=os.getcwd()+'\\ERP.xlsx'
-	rb = xlwt.Workbook()  #新建一个Excel
-	sheet = rb.add_sheet(u'translate',cell_overwrite_ok=True) #新建sheet
-	row = 1
-	while True:
-		next = f.readline()
-		if next != '' and re.match(r'^([0-9]).*',next):
-			tmp = next.split(' ')
-			re_chn = tmp[-1].strip('\n')
-			re_eng = " ".join('%s' %id for id in tmp[1:-1])
-			# print(re_chn)
-			# print(re_eng)
-			sheet.write(row,0,re_chn)
-			sheet.write(row,1,re_eng)
+			result = self.tran_google.google_translate(word_need)
+			print('当前第%s行'%row)
+			self.sheet.write(row, 2, result)
+			if re.search(r'[\u4e00-\u9fa5]',result):
+				print('未完整翻译')
+				self.sheet.write(row, 3, '未完整翻译')
 			row = row + 1
-		else:
-			break
-	rb.save(excel_path)
-	
-	#示例写入方法s
-
-
-def main():
-	#test()
-	# urls = get_url()
-	# parse_url(urls)
-	# we = Write_Excel(excel_path)
-	txt_to_excel()
-
+			# if row == 8:
+			# 	break
 
 
 if __name__ == '__main__':
-	main()
+	ERP_path = os.getcwd()+'\\ERP.xls'
+	tools = Translate_Excel(ERP_path)
+	tools.translate()
+	tools.save_excel()
